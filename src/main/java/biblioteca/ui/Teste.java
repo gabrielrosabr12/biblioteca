946ae91f26094494afffec09ace8864f4be828bb
Gabriel Rosa <gabrielrosaobr@gmail.com>
2025-10-26 13:06:06 -0300

    Atualizar o Teste.java

--- src/main/java/biblioteca/ui/Teste.java
+++ src/main/java/biblioteca/ui/Teste.java
@@ -6,8 +6,8 @@ import biblioteca.core.model.Usuario;
 public class Teste {
     public static void main(String[] args){
         Livro livro1 = new Livro("Moises","Java para iniciantes");
-        Livro livro2 = new Livro("Ana Beatriz","Fisioterapia dos bons");
-        Usuario user = new Usuario("10953357740","Gabriel");
+        Livro livro2 = new Livro("Creusa","Inicio em PHP");
+        Usuario user = new Usuario("108272747772","Gabriel");
 
         user.pegarEmprestado(livro1);
         user.pegarEmprestado(livro2);
